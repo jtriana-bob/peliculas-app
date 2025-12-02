450b8272cee35db9354435f4ab675b491d1a09aa
jhontriana <jtriana@kix.edu.ec>
2025-12-02 18:34:57 -0500

    modificaciones en detalles y color bg listado

--- src/components/detalles.jsx
+++ src/components/detalles.jsx
@@ -1,74 +1,67 @@
-import React, {useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {useParams} from "react-router";
-import {Link} from "react-router";
+import {PeliculasFavoritasContext} from "../providers/PeliculasFavoritasContext.js";
 
-const DetallePeliculas = () => {
+export default function PeliculaDetalle() {
+    const {id} = useParams()
 
-    const {id} = useParams();
+    const {
+        addPeliculaFavorita,
+        removePeliculaFavorita,
+        esFavorita
+    } = useContext(PeliculasFavoritasContext)
 
-    const [peliculas, setPeliculas] = React.useState(null);
-    const [cargando, setCargando] = React.useState(true);
+    const [pelicula, setPelicula] = useState(null)
+    const [loading, setLoading] = useState(true)
 
-    const getDetallePelicula =async () => {
-        setCargando(true);
+    const fetchPelicula = async () => {
         const response = await fetch(`https://imdb.iamidiotareyoutoo.com/search?tt=${id}`);
-
-        if (!response.ok) {
-            throw new Error(`Detalle pelicula ${id} no se encontro`);
-        }
-
         const data = await response.json();
-
-        //se pone short al revisar la API no es un array sino un objeto
-
-        setPeliculas(data.short);
-        setCargando(false);
+        setPelicula(data)
+        setLoading(false)
     }
 
     useEffect(() => {
-        getDetallePelicula();
-    }, [id])
+        fetchPelicula()
+    }, [id]);
 
-    if (cargando) {
-        return <div className={"absolute top-1/2 right-1/2"}><p>Cargando...</p></div>
-    }
+    if (!pelicula) return <p>Pelicula No encontrada</p>
+
+    if (loading) return <div>
+        Cargando...
+    </div>;
 
     return (
-
-        <div className="max-w-2xl m-auto p-6 bg-gray-200 rounded-2xl">
-            <Link to="/listado">Volver</Link>
-            <div className="flex flex-col justify-center items-center border rounded-lg p-4 m-4 bg-white shadow">
-                <div className="text-center ">
-                    <h1 className=" font-bold bg-black text-white inline-block rounded-2xl w-auto p-4">{peliculas.name}</h1>
-                </div>
-
-                <div>
-                    <div className="mx-4 flex justify-center">
-                        {peliculas.image && (
-                            <img src={peliculas.image} alt={peliculas.name} className="rounded-md mt-4 w-1/2" />
-                        )}
+        <div className="mt-10">
+            <div className="flex justify-center items-center mt-6">
+                <div className="rounded-2xl bg-gray-200 p-2 hover:scale-105 transition-all w-[500px] relative">
+                    <h2 className="text-amber-950 text-3xl">{pelicula.short.name}</h2>
+                    <div className="absolute top-1 right-5">
+                        {
+                            esFavorita(pelicula.imdbId) ?
+                                <img onClick={() => removePeliculaFavorita(pelicula.imdbId)} src="/star-on.svg"
+                                     alt="Star" width="40" height="40"/>
+                                : <img onClick={() => addPeliculaFavorita(pelicula)} src="/star-off.svg" alt="Star"
+                                       width="40" height="40"/>
+                        }
                     </div>
-                    <div className="gap-2 m-4">
-                        <p className="pb-5">{peliculas.description}</p>
-                        {peliculas.genre?.map((g, index) => (
-                            <span key={index} className="bg-blue-500 w-1/4 text-white text-xl capitalize p-3 m-4 ">
+                    <img className="rounded-2xl" src={pelicula.short.image} alt="Imagen Pelicula"/>
+                    <p className="text-center px-8 pt-4 mb-4">{pelicula.short.description}</p>
+                    {pelicula.short.genre?.map((g, index) => (
+                        <span key={index} className="bg-blue-500 w-[100px] h-[20px] text-white text-xl capitalize p-4 m-6 ">
                             {g}
                         </span>
-                        ))}
+                    ))}
+                    <div className="flex flex-wrap justify-center m-4 gap-4">
+                        <h3>Calificación</h3>
+                        {pelicula.short.review?.reviewRating ? (
+                            <p>{pelicula.short.review.reviewRating.ratingValue} / {pelicula.short.review.reviewRating.bestRating}</p>
+                        ) : (
+                            <p>No tiene calificación</p>
+                        )}
                     </div>
-                </div>
-                <div className="flex flex-wrap justify-center gap-2">
-                    <h3>Calificación</h3>
-                    {peliculas.review?.reviewRating ? (
-                        <p>{peliculas.review.reviewRating.ratingValue} / {peliculas.review.reviewRating.bestRating}</p>
-                    ) : (
-                        <p>No tiene calificación</p>
-                    )}
                 </div>
             </div>
         </div>
     );
-
-}
-
-export default DetallePeliculas;
+};
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Listado from "./components/listado.jsx";
 import DetallesPelicula from "./components/detalles.jsx";
 import { Navigate } from "react-router-dom";
+import {PeliculasFavoritasContext} from "./providers/PeliculasFavoritasContext.js";
+import PeliculasFavoritasProvider from "./providers/PeliculasFavoritasProvider.jsx";
 
 const router = createBrowserRouter([
     {
@@ -17,5 +19,8 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById('root')).render(
-    <RouterProvider router={router} />
+    <PeliculasFavoritasProvider>
+        <RouterProvider router={router}/>
+    </PeliculasFavoritasProvider>
+
 );
